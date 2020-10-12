934017b59a34f86c976ae99109d2de60f7eeca07
Sameer Saini <38816886+soyo-kaze@users.noreply.github.com>
2020-10-12 14:47:11 +0530

    Update Array.js

--- src/Array.js
+++ src/Array.js
@@ -30,6 +30,11 @@ const array = [
         name:"sam",
         description:"something2"
     }, 
+     {
+        img: dp,
+        name:"sam",
+        description:"something2"
+    }, 
 ];
 
 export default array;
